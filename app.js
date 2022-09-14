c078cd5dcf53ac55ff0d1881ee9bed569b47b2ee
Sadiqhasan <sadiqhasanrupani11@gmail.com>
2022-09-14 23:37:12 +0530

    Joke website using API

---
+++ app.js
@@ -0,0 +1,34 @@
+const resetBtn = document.querySelector("#reset-btn");
+const jokes = document.querySelector("#jokes");
+
+const addNewJoke = async () => {
+  const newJoke = await dadsJoke();
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.classList.add("joke-container");
+  li.classList.add("jokeList");
+  li.textContent = newJoke;
+  jokes.append(div);
+  div.append(li);
+};
+
+// dadjoke api
+const dadsJoke = async () => {
+  const config = { headers: { Accept: "application/json" } };
+  const response = await axios.get("https://icanhazdadjoke.com/", config);
+  return response.data.joke;
+};
+
+for (let i = 0; i < 10; i++) {
+  addNewJoke();
+}
+
+resetBtn.addEventListener("click", () => {
+  let divs = document.querySelectorAll(".joke-container");
+  for (di of divs) {
+    di.remove();
+  }
+  for (let i = 0; i < 10; i++) {
+    addNewJoke();
+  }
+});
